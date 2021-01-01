46e1d4f63d898e01418e83f14d01f685c86a2332
gyanesh-mahana <gkm010@gmail.com>
2021-01-01 20:18:06 +0530

    updated comment for the method

--- ChapterLinkedList/src/singlyLinkedList/LinkedListCycle.java
+++ ChapterLinkedList/src/singlyLinkedList/LinkedListCycle.java
@@ -1,7 +1,7 @@
 /*
  * Double pointer Concept
  * ****************************
- * Problem: Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * Problem 1: Given head, the head of a linked list, determine if the linked list has a cycle in it.
  * 			There is a cycle in a linked list if there is some node in the list that can be reached 
  * 			again by continuously following the next pointer. Internally, pos is used to denote the 
  * 			index of the node that tail's next pointer is connected to. Note that pos is not passed
@@ -9,7 +9,15 @@
  *  		Return true if there is a cycle in the linked list. Otherwise, return false.
  *  
  * Link: https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1212/
- * 
+ * ****************************
+ * Problem 2: Given a linked list, return the node where the cycle begins. If there is no cycle, 
+ * 			return null. There is a cycle in a linked list if there is some node in the list that
+ * 			can be reached again by continuously following the next pointer. Internally, pos is 
+ * 			used to denote the index of the node that tail's next pointer is connected to. Note 
+ * 			that pos is not passed as a parameter.Notice that you should not modify the linked list.
+ *  
+ * Link: https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1214/
+ * ****************************
  */
 
 package singlyLinkedList;
